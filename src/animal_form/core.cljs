49f0899774c3f1aff3c8e7f89f5c63cc9a65ca7f
marvin ma <marvin.ma@redcreation.net>
2022-10-26 22:05:54 +0800

    init

---
+++ src/animal_form/core.cljs
@@ -0,0 +1,73 @@
+(ns animal-form.core
+  (:require
+   [reagent.dom :as rdom]
+   [re-frame.core :as re-frame]
+   [animal-form.events :as events]
+   [animal-form.views :as views]
+   [animal-form.config :as config]
+   [kee-frame.core :as kf]
+   ))
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn login-page []
+  [:div {:style {:width "300px"
+                 :height "200px"
+                 :position "absolute"
+                 :margin "auto"
+                 :top 0
+                 :bottom 0
+                 :left 0
+                 :right 0
+                 }}
+   [:div.field
+    [:label.label "用户名"]
+    [:div.control
+     [:input.input {:type "text" :placeholder "请填写用户名"}]]
+    [:label.label "密码"]
+    [:div.control
+     [:input.input {:type "text" :placeholder "请填写密码"}]]
+    [:button.button.is-primary
+     {:on-click #(re-frame/dispatch [::events/to-index])}
+     "登录"]]])
+
+(defn loading-page []
+  [:div "loading"])
+
+(def routes
+  [["/" :index]
+   ["/login" :login]
+   ["/main*path" :main]])
+
+(defn root-component []
+  [:div
+   [kf/switch-route (fn [route] (get-in route [:data :name]))
+    :login [login-page]
+    :index [views/main-panel]
+    nil [loading-page]]])
+
+(kf/reg-controller
+ :league
+ {:params (fn [route-data]
+            (when (-> route-data :data :name (= :league))
+              true))
+  :start  (fn [ctx _]
+            (prn "进入start"))})
+
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (let [root-el (.getElementById js/document "app")]
+    (rdom/unmount-component-at-node root-el)
+    (kf/start!  {:routes         routes
+                 :initial-db     {:some-prop true}
+                 :root-component [root-component]
+                 :debug?         true})
+    #_(rdom/render [views/main-panel] root-el)))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
---
+++ src/animal_form/events.cljs
@@ -0,0 +1,32 @@
+(ns animal-form.events
+  (:require
+   [re-frame.core :as re-frame]
+   [kee-frame.core :as kf]
+   [animal-form.db :as db]
+   [day8.re-frame.tracing :refer-macros [fn-traced]]
+   ))
+
+(re-frame/reg-event-db
+ ::initialize-db
+ (fn-traced [_ _]
+            db/default-db))
+
+(re-frame/reg-event-db
+ ::update-form
+ (fn [db [_ id val]]
+   (assoc-in db [:form id] val)))
+
+(re-frame/reg-event-db
+ ::save-form
+ (fn [db]
+   (let [form-data (:form db)
+         animals (get db :animals [])
+         updated-animals (conj animals form-data)]
+     (-> db
+         (assoc :animals updated-animals)
+         (dissoc :form)))))
+
+(kf/reg-event-fx
+ ::to-index
+ (fn []
+   {:navigate-to [:index]}))
